ae61f79ec19419fe0b1518f4d257f1adaba226b0
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-10-24 13:25:18 +0200

    RestControllerAdvice annotation is used instead of ControllerAdvice annotation lest to get ServletException

--- src/main/java/com/codeoftheweb/salvo/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/codeoftheweb/salvo/exception/handler/GlobalExceptionHandler.java
@@ -3,15 +3,14 @@ package com.codeoftheweb.salvo.exception.handler;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
