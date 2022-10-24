a3f5e46930925dcceed492332801ac10301390f2
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2022-10-24 13:49:29 +0200

    When RestControllerAdvice is used we do not have to return ResponseEntity, that was why we were having ServletException when we had ControllerAdvice annotation because we were returning Map<> instead of ResponseEntity<Map<>>. RestControllerAdvice is handling the ResponseEntity object. Now we put the ControllerAdvice annotation back and we returned ResponseEntity<Map>> and it works fine now without Servlet exception.

--- src/main/java/com/codeoftheweb/salvo/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/codeoftheweb/salvo/exception/handler/GlobalExceptionHandler.java
@@ -1,20 +1,20 @@
 package com.codeoftheweb.salvo.exception.handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
@@ -22,6 +22,6 @@ public class GlobalExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return errors;
+        return ResponseEntity.badRequest().body(errors);
     }
 }
