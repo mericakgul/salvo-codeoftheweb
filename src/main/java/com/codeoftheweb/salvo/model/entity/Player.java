1c96c90bf75658a90abce9d5ce2d2ee7751b33a9
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2023-01-03 00:11:27 +0100

    Added Set of gamePlayers to Player entity

--- src/main/java/com/codeoftheweb/salvo/model/entity/Player.java
+++ src/main/java/com/codeoftheweb/salvo/model/entity/Player.java
@@ -1,10 +1,13 @@
 package com.codeoftheweb.salvo.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -17,6 +20,10 @@ public class Player {
     private Long id;
     @Column(unique = true)
     private String username;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "player")
+    private Set<GamePlayer> gamePlayers = new HashSet<>();
 
     public Player(String username){
         this.username = username;
