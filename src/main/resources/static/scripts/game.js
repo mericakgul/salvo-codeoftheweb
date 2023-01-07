2345ddbe00a82a0d8619ba1de35a9fe9423bd51d
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2023-01-07 21:49:31 +0100

    game_view page has been added. According to given url parameter we fetch the correct gamePlayer data and show the game info and ship locations of the viewer (the player in selected gamePlayer).

--- src/main/resources/static/scripts/game.js
+++ src/main/resources/static/scripts/game.js
@@ -1,7 +1,14 @@
+import {fetchJson} from "./utilities/helpers.js";
+
 const gridContainer = document.querySelector('.grid-container');
 const gridSize = 10;
 gridContainer.setAttribute('style', `grid-template-columns:repeat(${gridSize + 1}, 1fr)`); // To be able to have dynamic grid size in case we want different size of grid.
 let rowLetter = 'A'; // The beginning letter of the row.
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+const gamePlayerId = params['gp'];
 
 createGrid();
 
@@ -44,4 +51,31 @@ function createRowCells() {
 
 function nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
+}
+
+if (gamePlayerId !== null) {
+        fetchJson(`/api/game_view/${gamePlayerId}`).then((game) => {
+            placeShipsOnGrid(game['ships']);
+            showGameInfo(game['gamePlayers']);
+        });
+}
+
+function placeShipsOnGrid(ships) {
+    ships.forEach(ship => {
+        ship['locations'].forEach(location => {
+            const gridCell = document.querySelector(`#${location}`);
+            gridCell.setAttribute('style', 'background-color: rgba(12, 25, 25, 0.8)');
+        });
+    });
+}
+
+function showGameInfo(gamePlayers) {
+    const gamePlayerViewer = gamePlayers.find(({id}) => id.toString() === gamePlayerId);
+    const viewerUsername = gamePlayerViewer['player']['username'];
+    const gamePlayerOpponent = gamePlayers.find(({id}) => id.toString() !== gamePlayerId);
+    const opponentUsername = gamePlayerOpponent === undefined ? '"waiting_for_opponent"' : gamePlayerOpponent['player']['username'];
+
+    const gameInfoTextField = document.querySelector('#game-info');
+    const gameInfoText = document.createTextNode(`${viewerUsername} (you) vs ${opponentUsername}`);
+    gameInfoTextField.appendChild(gameInfoText);
 }
