6e2efefe36a03564f818f9fc2a00668b7726d857
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2023-01-25 18:01:28 +0100

    Simple version of authentication and authorisation added and working, however deprecated class WebSecurityConfigurerAdapter has been used. We can update it later.

--- src/main/java/com/codeoftheweb/salvo/model/entity/Player.java
+++ src/main/java/com/codeoftheweb/salvo/model/entity/Player.java
@@ -4,18 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
 @Table(name = "players")
 @Setter
 @NoArgsConstructor
-public class Player implements UserDetails {
+public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,30 +39,5 @@ public class Player implements UserDetails {
     public Player(String username, String password){
         this.username = username;
         this.password = password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
