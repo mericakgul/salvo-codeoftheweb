f36ca4b17c4f94fd072897bcc5ce342ee8276e18
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2023-03-30 15:58:42 +0200

    comment added for study purposes

--- src/main/resources/static/scripts/utilities/helpers.js
+++ src/main/resources/static/scripts/utilities/helpers.js
@@ -59,6 +59,10 @@ function areFieldsValid() {
     return true;
 }
 
+// While sending log in request we send the file in application/x-www-form-urlencoded format not json format. This is why body value here is different from the body value in signUp request.
+// Here we use evt parameter, and we reach the form with evt.target.form value. Afterwards, username and password variables are assigned to form.username.value and form.password.value values respectively.
+// How does it understand which input fields in the form are for username and password? HtmlFormElement checks the input elements with 'name' (or 'id') attribute valued as username and password and takes the input values(provided by user) in these inputs and assigns it in body part of the request.
+// Another thing is the login request done to /api/login doesn't have any endpoint we created manually. It is provided in the web security configuration ( http.formLogin().loginProcessingUrl("/api/login") )
 function sendLoginRequest(evt) {
     const form = evt.target.form;
     fetch('/api/login', {
@@ -77,6 +81,9 @@ function sendLoginRequest(evt) {
     });
 }
 
+// Here in signUp request body we do not use evt parameter and HtmlFormElement(evt.target.form) because our format is application/json,
+// so we just find the username and password input fields by using query selectors and take the input values in it and place them to our json in the body part
+// and make a signup POST request to the endpoint that we manually created which is'/api/players'
 function sendSignupRequest(evt) {
     fetch('/api/players', {
         method: 'POST',
