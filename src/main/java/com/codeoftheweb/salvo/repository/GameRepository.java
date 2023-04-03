f4422729e17998f91652f30f15b8734c4eda9b38
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2023-04-03 16:07:32 +0200

    removed unused import

--- src/main/java/com/codeoftheweb/salvo/repository/GameRepository.java
+++ src/main/java/com/codeoftheweb/salvo/repository/GameRepository.java
@@ -1,7 +1,6 @@
 package com.codeoftheweb.salvo.repository;
 
 import com.codeoftheweb.salvo.model.entity.Game;
-import com.codeoftheweb.salvo.model.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
