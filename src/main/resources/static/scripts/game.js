f72c08baf3c1cb103b086a8d38a613a5ccdc3eda
Meriç Akgül <39882155+mericakgul@users.noreply.github.com>
2023-04-14 00:35:47 +0200

    - Remove and Save buttons have been added. - Click event listener has been added for remove button. Selected ship is removed from map and from placed ships checkbox list. It is enabled back in radi buttons ship list.

--- src/main/resources/static/scripts/game.js
+++ src/main/resources/static/scripts/game.js
@@ -11,13 +11,14 @@ const salvoesGridContainer = document.querySelector('#salvoes-grid-container');
 const loggedInPlayerUsernameArea = document.querySelector('#logged-in-player');
 const logoutBtn = document.querySelector('#logout-btn');
 const placedShipsContainer = document.querySelector('.placed-ships-container');
+const removeShipButton = document.querySelector('#removeShip');
+const saveShipButton = document.querySelector('#saveShip');
 
 shipsGridContainer.setAttribute('style', `grid-template-columns:repeat(${gridSize + 1}, 1fr)`); // To be able to have dynamic grid size in case we want different size of grid.
 salvoesGridContainer.setAttribute('style', `grid-template-columns:repeat(${gridSize + 1}, 1fr)`);
 let rowLetterShip = 'A'; // The beginning letter of the row.
 let rowLetterSalvo = 'A';
 let shipObjectListPlacedByUser = [];
-console.log('shipObjectListPlacedByUser', shipObjectListPlacedByUser);
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
@@ -25,7 +26,6 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 const gamePlayerId = params['gp'];
 const fetchedGameView = await fetchGameViewObject(gamePlayerId);
 const allLocationsOfAlreadySentOwnerShips = combineShipsLocations(fetchedGameView['ships']);
-console.log('allLocationsOfAlreadySentOwnerShips', allLocationsOfAlreadySentOwnerShips);
 
 if (loggedInPlayerUsername) {
     showPlayerUsername(loggedInPlayerUsername, loggedInPlayerUsernameArea);
@@ -80,128 +80,6 @@ function createRowCells(gridContainer, rowLetter, gridItemCallBack) {
     rowLetter === rowLetterShip ?
         rowLetterShip = nextChar(rowLetterShip) :
         rowLetterSalvo = nextChar(rowLetterSalvo);
-}
-
-function handleShipGridItemClick(event) {
-    const clickedItemId = event.target.id;
-    const clickedItemGridCode = clickedItemId.slice(4);
-    console.log('clickedItemId', clickedItemId);
-    console.log('clickedItemGridCode', clickedItemGridCode);
-    const selectedShip = getSelectedShip();
-    const selectedDirection = getSelectedDirection();
-    if (!selectedShip || !selectedDirection) {
-        alert('Please select a ship and direction from the menu to place');
-    } else {
-        const selectedShipData = allShipTypes
-            .find(ship => ship.id === selectedShip);
-        const shipSize = selectedShipData?.size;
-        const shipName = selectedShipData?.name;
-        const isThereEnoughSpace = checkIfThereIsEnoughSpace(shipSize, selectedDirection, clickedItemGridCode);
-        if (isThereEnoughSpace) {
-            const shipObject = createShipObject(shipName, shipSize, selectedDirection, clickedItemGridCode);
-            console.log('shipObject', shipObject);
-            placeSelectedShipOnGrid(shipObject, selectedShipData);
-        } else {
-            alert('Ship cannot be placed.');
-        }
-        console.log('selectedShipData', selectedShipData);
-        console.log('selectedShip', selectedShip);
-        console.log('shipSize', shipSize);
-        console.log('selectedDirection', selectedDirection);
-    }
-}
-
-function checkIfThereIsEnoughSpace(shipSize, selectedDirection, clickedItemGridCode) {
-    if (selectedDirection === 'vertical') {
-        const clickedGridLetter = clickedItemGridCode[0];
-        const numLettersToEnd = lastLetterInMap.charCodeAt(0) - clickedGridLetter.charCodeAt(0) + 1;
-        return numLettersToEnd >= shipSize;
-    } else {
-        const clickedGridNumber = parseInt(clickedItemGridCode.substring(1), 10);
-        const numDigitsToEnd = gridSize - clickedGridNumber + 1;
-        return numDigitsToEnd >= shipSize;
-    }
-}
-
-function createShipObject(shipName, shipSize, selectedDirection, clickedItemGridCode) {
-    let shipObject;
-    const shipLocationsArray = createShipLocationsArray(clickedItemGridCode, shipSize, selectedDirection);
-    shipObject = {
-        "shipType": shipName,
-        "shipLocations": shipLocationsArray
-    };
-    return shipObject;
-}
-
-function createShipLocationsArray(clickedItemGridCode, shipSize, selectedDirection) {
-    const clickedGridNumber = parseInt(clickedItemGridCode.substring(1), 10);
-    const clickedGridLetter = clickedItemGridCode[0];
-
-    if (selectedDirection === 'vertical') {
-        const startCharCode = clickedGridLetter.charCodeAt(0);
-        return Array.from(
-            {length: shipSize},
-            (_, index) => String.fromCharCode(startCharCode + index) + clickedGridNumber.toString()
-        );
-    } else {
-        return Array.from(
-            {length: shipSize},
-            (_, index) => clickedGridLetter + (clickedGridNumber + index).toString()
-        );
-    }
-}
-
-function getSelectedShip() {
-    const shipsToPlace = document.getElementsByName("shipsToPlace");
-    let checkedShipId = null;
-    Array.from(shipsToPlace).some((radioButton) => {
-        if (radioButton.checked) {
-            checkedShipId = radioButton.id;
-            return true;
-        }
-    });
-    return checkedShipId?.split('_')[0];
-}
-
-function getSelectedDirection() {
-    const shipDirections = document.getElementsByName("shipDirection");
-    let checkedShipDirection = null;
-    Array.from(shipDirections).some(radioButton => {
-        if (radioButton.checked) {
-            checkedShipDirection = radioButton.id;
-        }
-    });
-    return checkedShipDirection;
-}
-
-function placeSelectedShipOnGrid (ship, selectedShipData){
-    const allLocationsOfPlacedShips = combineShipsLocations(shipObjectListPlacedByUser);
-    console.log('allLocationsOfPlacedShips', allLocationsOfPlacedShips);
-    const combinedSavedAndPlacedShipLocations = [...allLocationsOfAlreadySentOwnerShips, ...allLocationsOfPlacedShips];
-    console.log('combinedSavedAndPlacedShipLocations', combinedSavedAndPlacedShipLocations);
-    const isOverlap = isShipOverlap(ship, combinedSavedAndPlacedShipLocations);
-    if(isOverlap) {
-        alert('Ships are overlapping, select another point.');
-    } else {
-        ship['shipLocations'].forEach(location => {
-            const gridCell = document.querySelector(`#SHIP${location}`);
-            gridCell ?
-                gridCell.setAttribute('style', 'background-color: lightblue') :
-                alert(`The location ${location} is not exist in the grid table. Check your locations.`);
-        });
-        shipObjectListPlacedByUser.push(ship);
-        createPlacedShipsCheckBoxes(selectedShipData);
-    }
-}
-
-function isShipOverlap(ship, combinedSavedAndPlacedShipLocations) {
-    const selectedShipLocations = ship['shipLocations'];
-    const allLocationsSet = new Set([...selectedShipLocations, ...combinedSavedAndPlacedShipLocations]);
-    return allLocationsSet.size < selectedShipLocations.length + combinedSavedAndPlacedShipLocations.length;
-}
-
-function handleSalvoGridItemClick() {
-    console.log('Salvo grid item click handled');
 }
 
 function placeDataOnGrids() {
@@ -302,35 +180,207 @@ function createShipsForms(game) {
     });
 }
 
-function createInput(ship, type) {
+function createInput(selectedShipData, type) {
     const input = document.createElement('input');
     input.classList.add('form-check-input');
     input.setAttribute('type', type);
-    input.setAttribute('id', `${ship.id}_${type}`);
+    input.setAttribute('id', `${selectedShipData.id}_${type}`);
     if (type === 'radio') {
         input.setAttribute('name', 'shipsToPlace');
     }
     return input;
 }
 
-function createLabel(ship, type) {
+function createLabel(selectedShipData, type) {
     const label = document.createElement('label');
     label.classList.add('form-check-label');
-    label.setAttribute('for', `${ship.id}_${type}`);
-    label.textContent = `${ship.name} (${'\u25A0 '.repeat(ship.size)})`;
+    label.setAttribute('for', `${selectedShipData.id}_${type}`);
+    label.textContent = `${selectedShipData.name} (${'\u25A0 '.repeat(selectedShipData.size)})`;
     return label;
 }
 
-function createPlacedShipsCheckBoxes(ship) {
+function handleShipGridItemClick(event) {
+    const clickedItemId = event.target.id;
+    const clickedItemGridCode = clickedItemId.slice(4);
+    const selectedShip = getSelectedShip();
+    const selectedDirection = getSelectedDirection();
+    if (!selectedShip || !selectedDirection) {
+        alert('Please select a ship and direction from the menu to place');
+    } else {
+        const selectedShipData = allShipTypes
+            .find(ship => ship.id === selectedShip);
+        const shipSize = selectedShipData?.size;
+        const shipName = selectedShipData?.name;
+        const isThereEnoughSpace = checkIfThereIsEnoughSpace(shipSize, selectedDirection, clickedItemGridCode);
+        if (isThereEnoughSpace) {
+            const shipObject = createShipObject(shipName, shipSize, selectedDirection, clickedItemGridCode);
+            placeSelectedShipOnGrid(shipObject, selectedShipData);
+        } else {
+            alert('Ship cannot be placed.');
+        }
+    }
+}
+
+function checkIfThereIsEnoughSpace(shipSize, selectedDirection, clickedItemGridCode) {
+    if (selectedDirection === 'vertical') {
+        const clickedGridLetter = clickedItemGridCode[0];
+        const numLettersToEnd = lastLetterInMap.charCodeAt(0) - clickedGridLetter.charCodeAt(0) + 1;
+        return numLettersToEnd >= shipSize;
+    } else {
+        const clickedGridNumber = parseInt(clickedItemGridCode.substring(1), 10);
+        const numDigitsToEnd = gridSize - clickedGridNumber + 1;
+        return numDigitsToEnd >= shipSize;
+    }
+}
+
+function createShipObject(shipName, shipSize, selectedDirection, clickedItemGridCode) {
+    let shipObject;
+    const shipLocationsArray = createShipLocationsArray(clickedItemGridCode, shipSize, selectedDirection);
+    shipObject = {
+        "shipType": shipName,
+        "shipLocations": shipLocationsArray
+    };
+    return shipObject;
+}
+
+function createShipLocationsArray(clickedItemGridCode, shipSize, selectedDirection) {
+    const clickedGridNumber = parseInt(clickedItemGridCode.substring(1), 10);
+    const clickedGridLetter = clickedItemGridCode[0];
+
+    if (selectedDirection === 'vertical') {
+        const startCharCode = clickedGridLetter.charCodeAt(0);
+        return Array.from(
+            {length: shipSize},
+            (_, index) => String.fromCharCode(startCharCode + index) + clickedGridNumber.toString()
+        );
+    } else {
+        return Array.from(
+            {length: shipSize},
+            (_, index) => clickedGridLetter + (clickedGridNumber + index).toString()
+        );
+    }
+}
+
+function getSelectedShip() {
+    const shipsToPlace = document.getElementsByName("shipsToPlace");
+    let checkedShipId = null;
+    Array.from(shipsToPlace).some((radioButton) => {
+        if (radioButton.checked) {
+            checkedShipId = radioButton.id;
+            return true;
+        }
+    });
+    return checkedShipId?.split('_')[0];
+}
+
+function getSelectedDirection() {
+    const shipDirections = document.getElementsByName("shipDirection");
+    let checkedShipDirection = null;
+    Array.from(shipDirections).some(radioButton => {
+        if (radioButton.checked) {
+            checkedShipDirection = radioButton.id;
+        }
+    });
+    return checkedShipDirection;
+}
+
+function placeSelectedShipOnGrid(shipObject, selectedShipData) {
+    const allLocationsOfPlacedShips = combineShipsLocations(shipObjectListPlacedByUser);
+    const combinedSavedAndPlacedShipLocations = [...allLocationsOfAlreadySentOwnerShips, ...allLocationsOfPlacedShips];
+    const isOverlap = isShipOverlap(shipObject, combinedSavedAndPlacedShipLocations);
+    if (isOverlap) {
+        alert('Ships are overlapping, select another point.');
+    } else {
+        shipObject['shipLocations'].forEach(location => {
+            const gridCell = document.querySelector(`#SHIP${location}`);
+            gridCell ?
+                gridCell.setAttribute('style', 'background-color: lightskyblue') :
+                alert(`The location ${location} is not exist in the grid table. Check your locations.`);
+        });
+        shipObjectListPlacedByUser.push(shipObject);
+        createPlacedShipsCheckBoxes(selectedShipData);
+    }
+}
+
+function isShipOverlap(shipObject, combinedSavedAndPlacedShipLocations) {
+    const selectedShipLocations = shipObject['shipLocations'];
+    const allLocationsSet = new Set([...selectedShipLocations, ...combinedSavedAndPlacedShipLocations]);
+    return allLocationsSet.size < selectedShipLocations.length + combinedSavedAndPlacedShipLocations.length;
+}
+
+function createPlacedShipsCheckBoxes(selectedShipData) {
     const container = document.createElement('div');
     container.classList.add('form-check');
-    const input = createInput(ship, 'checkbox');
-    const label = createLabel(ship, 'checkbox');
-    const placedShipRadioButton = document.querySelector(`#${ship.id}_radio`);
-    placedShipRadioButton.checked = false;
-    placedShipRadioButton.disabled = true;
-
+    const input = createInput(selectedShipData, 'checkbox');
+    const label = createLabel(selectedShipData, 'checkbox');
+    input.addEventListener('click', updateRemoveAndSaveButtons);
     container.appendChild(input);
     container.appendChild(label);
     placedShipsContainer.appendChild(container);
+
+    const placedShipRadioButton = document.querySelector(`#${selectedShipData.id}_radio`);
+    placedShipRadioButton.checked = false;
+    placedShipRadioButton.disabled = true;
+}
+
+function updateRemoveAndSaveButtons() {
+    const checkboxes = placedShipsContainer.querySelectorAll('input[type="checkbox"]');
+    const isAnyShipChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+    removeShipButton.disabled = !isAnyShipChecked;
+    saveShipButton.disabled = !isAnyShipChecked;
+}
+
+removeShipButton.addEventListener('click', removeCheckedShip);
+
+function removeCheckedShip() {
+    const checkedShipIds = getCheckedShipIds();
+    checkedShipIds.forEach(shipId => {
+        removeShipFromCheckboxShipList(shipId);
+        enableShipOnRadioShipList(shipId);
+        removeShip(shipId);
+        updateRemoveAndSaveButtons();
+    });
+}
+
+function getCheckedShipIds() {
+    const checkboxes = placedShipsContainer.querySelectorAll('input[type="checkbox"]');
+    return Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.id.split('_')[0]);
+}
+
+function removeShipFromCheckboxShipList(shipId) {
+    const checkedShip = document.getElementById(shipId + '_checkbox');
+    const parentElementOfCheckedShip = checkedShip.closest('.form-check');
+    parentElementOfCheckedShip.remove();
+}
+
+function enableShipOnRadioShipList(shipId) {
+    const shipRadioButton = document.getElementById(shipId + '_radio');
+    shipRadioButton.disabled = false;
+}
+
+function removeShip(shipId) {
+    const shipName = allShipTypes
+        .find(ship => ship.id === shipId).name;
+    const shipObjectToRemove = shipObjectListPlacedByUser.find(shipObject => shipObject['shipType'] === shipName);
+    const locationsShipObjectToRemove = shipObjectToRemove['shipLocations'];
+    deleteShipFromMap(locationsShipObjectToRemove);
+
+    const indexToRemove = shipObjectListPlacedByUser.findIndex(shipObject => shipObject['shipType'] === shipName);
+    if(indexToRemove !== -1){
+        shipObjectListPlacedByUser.splice(indexToRemove, 1);
+    }
+}
+
+function deleteShipFromMap(locationsShipObjectToRemove){
+    locationsShipObjectToRemove.forEach(location => {
+        const gridCellId = 'SHIP' + location;
+        const gridCell = document.getElementById(gridCellId);
+        gridCell.setAttribute('style', 'background-color: lightgray');
+    });
+}
+
+function handleSalvoGridItemClick() {
+    console.log('Salvo grid item click handled');
 }
